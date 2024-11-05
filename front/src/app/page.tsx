9d805b81d7636c82e72ac66fa9b9f94c7d1d1cb5
hndrr <o_honda@go-spiral.ai>
2024-11-05 15:19:46 +0900

    Refactor form layout and styles

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -5,21 +5,21 @@ import { SourceMessage } from "@/components/SourceMessage";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main className="flex items-center flex-col">
         <div className="flex-1">
           <QuestionMessage content="Dummy Question" />
           <SearchMessage content="Dummy Search Message" />
           <SourceMessage sources={["https://example.com"]} />
           <AnswerMessage content="Dummy Answer Message" />
         </div>
-          <form>
-            <input 
-              type="text"
-              name="message"
-              placeholder="テキストを入力"
-              className="border-2 border-gray-300 p-2 rounded-lg" />
-          </form>
+        <form>
+          <input 
+            type="text"
+            name="message"
+            placeholder="テキストを入力"
+            className="border-2 border-gray-300 p-2 rounded-lg" />
+        </form>
       </main>
     </div>
   );
