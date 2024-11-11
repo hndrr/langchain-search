b1981e285f8955b9c70ad2b451764a0870edfc3d
hndrr <o_honda@go-spiral.ai>
2024-11-11 19:32:57 +0900

    Refactor imports and add Turso database configuration

--- llm/data/database.py
+++ llm/data/database.py
@@ -1,3 +1,4 @@
+import config as config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,11 +11,14 @@ Base = declarative_base()
 base_dir = os.path.dirname(__file__)
 
 # SQLiteデータベースのURLを設定
-DATABASE_URL = "sqlite:///" + os.path.join(base_dir, "fastapi_app.db")
+## DATABASE_URL = "sqlite:///" + os.path.join(base_dir, "fastapi_app.db")
+
+# TursoのデータベースURLを設定
+DATABASE_URL = f"sqlite+{config.TURSO_DATABASE_URL}/?authToken={config.TURSO_AUTH_TOKEN}&secure=true"
 
 # SQLiteデータベースのエンジンを作成
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    DATABASE_URL, connect_args={'check_same_thread': False}, echo=True)
+
 # セッションローカルを作成
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
